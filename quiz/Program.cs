ef10c0de2205676ca70c84d4f4f0dc7c4f94ed50
Kasyanov-Igor <igor.kasyanov00@mail.ru>
2024-09-07 21:14:53 +0300

    Added Menu. Closed #10

--- quiz/Program.cs
+++ quiz/Program.cs
@@ -2,3 +2,17 @@
 
 Quiz quizGame = new Quiz();
 
+while (true)
+{
+	if (quizGame.CheckingUserExistence())
+	{
+		quizGame.PrintMenu();
+	}
+	Console.WriteLine("User does not exist, please register\n register?");
+	string? UserAnswer = Console.ReadLine();
+	if (UserAnswer == "yes")
+	{
+		quizGame.Registration();
+	}
+	Console.Clear();
+}
